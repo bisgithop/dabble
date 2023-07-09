096005c0dba13bee11790c3fb6bab98c5fe0990b
bisgithop <139035386+bisgithop@users.noreply.github.com>
2023-07-09 22:13:29 +0530

    Add files via upload

---
+++ huggingface.py
@@ -0,0 +1,15 @@
+import json
+import requests
+API_TOKEN = "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"
+
+API_URL = "https://api-inference.huggingface.co/models/openai/gpt-3.5-turbo"
+#API_URL = "https://api-inference.huggingface.co/models/gpt2-xl"
+headers = {"Authorization": f"Bearer {API_TOKEN}"}
+def query(payload):
+    data = json.dumps(payload)
+    response = requests.request("POST", API_URL, headers=headers, data=data)
+    return json.loads(response.content.decode("utf-8"))
+
+data = query("hi")
+for i in data:
+    print(data)
